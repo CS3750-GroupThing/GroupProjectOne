94855d9ef4fb3b8394ea2bac85418227385fa248
AaronLL <aaron@tadoo.net>
2016-03-07 09:04:36 -0700

    Update db.php

--- www/php/db.php
+++ www/php/db.php
@@ -1,6 +1,6 @@
 <?php
 //switch ($_SERVER['HTTP_ORIGIN']) {
-	case 'http://from.com': case 'https://from.com':
+	//case 'http://from.com': case 'https://from.com':
 	header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
 	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 	header('Access-Control-Max-Age: 1000');
