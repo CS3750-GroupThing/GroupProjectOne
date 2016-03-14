f92a8f1964ac58c5c8259e26d3ff89f362b2bdff
AaronLL <aaron@tadoo.net>
2016-03-14 17:14:12 -0600

    Added files via upload

--- www/js/db.js
+++ www/js/db.js
@@ -1,5 +1,12 @@
-function syncDB()
+function syncDBTX(tx)
 {
+	tx.executeSql("SELECT * FROM todo_Items WHERE owner=" + owner_ID + " ORDER BY id DESC", [], syncDB, errorCB);
+
+}
+
+function syncDB(tx, results)
+{
+	var jsonTodoItems;
 	var url = "http://icarus.cs.weber.edu/~al53910/db.php";
 	var userName = "TestOwner";
 	var passWord = "7faa7303f769ce1d1f9c88bb669fbad62320c09eb131a47d78119842918853235f862caf9d9b2d6191cb03dad28609605c3f9298a8b81ab68674253e9e71d076";
@@ -12,41 +19,91 @@ function syncDB()
 	{
 		username: userName,
 		password: passWord,
-		action: doAction
+		action: GET
 	},
 	function(data,status){
 		if (status == "success")
 		{
 			/*console.log("Data: " + data + "\nStatus: " + status);*/
-			compareJSON(data);
+		    /* ##############################################################################################################
+				Adding any remote todo items to the local database which are missing
+                       ############################################################################################################## */
+			var jsonItems = JSON.parse(data);
+			var found = false;
+
+			//console.log("Trying to parse JSON: " + jsonTodoItems + " with a length of " + jsonItems.length);
+			for(var i = 0;i<jsonItems.length;i++)
+			{
+				console.log("Searching retrieved JSON values for a hash matching: " + jsonItems[i].hash);
+				//console.log("JSON Item: " + i + " - hash: " + jsonItems[i].hash + " - timedate: " + jsonItems[i].timedate + " - completed: " + jsonItems[i].completed + " - description: " + jsonItems[i].description);
+
+				for(var j = 0;j<results.rows.length;j++)
+				{
+					if (jsonItems[i].hash == results.rows.item(j).hash)
+					{
+						console.log("Match found for : " + jsonItems[i].hash + ". Checking for updates.");
+						if (jsonItems[i].completed != results.rows.item(j).completed)
+						{
+							completeToDo(results.rows.item(j).id, jsonItems[i].completed)
+						}
+						found = true;
+					}
+				}
+				if (found == false)
+				{
+					console.log("No match found for : " + jsonItems[i].hash + ".  Inserting into local database.");
+					addTodoFromRemote(jsonItems[i].hash, jsonItems[i].timedate, jsonItems[i].completed, jsonItems[i].description);
+					//tx.executeSql("INSERT INTO todo_Items (hash, owner, completed, description) VALUES ('" + jsonItems[i].hash + "', "+ owner_ID + ", " + jsonItems[i].completed + ", '" + jsonItems[i].description + "')");
+				}
+				found = false;
+			}
+
+		    /* ##############################################################################################################
+				Adding any local todo items to the remote database which are missing
+                       ############################################################################################################## */
+			/*var jsonItems = JSON.parse(data);
+			var found = false;
+
+			//console.log("Trying to parse JSON: " + jsonTodoItems + " with a length of " + jsonItems.length);
+			for(var i = 0;i<results.rows.length;i++)
+			{
+				console.log("Searching through retrieved JSON values for a hash matching: " + results.rows.item(i).hash);
+
+				for(var j = 0;j<jsonItems.length;j++)
+				{
+					if (jsonItems[j].hash == results.rows.item(i).hash)
+					{
+						console.log("Match found for : " + jsonItems[j].hash + ". Checking for updates.");
+						if (jsonItems[j].completed != results.rows.item(i).completed)
+						{
+							completeToDo(results.rows.item(i).id, jsonItems[j].completed)
+						}
+						found = true;
+					}
+				}
+				if (found == false)
+				{
+					console.log("No match found for : " + results.rows.item(j).hash + ".  Inserting into remote database.");
+					$.post("http://icarus.cs.weber.edu/~al53910/db.php",
+					{
+						username: userName,
+						password: passWord,
+						action: ADD,
+						hash: results.rows.item(j).hash,
+						completed: results.rows.item(j).completed,
+						timedate: hash: results.rows.item(j).timedate
+						
+					},
+					function(data,status){
+						// Do nothing as it will correct itself on the next sync.	
+					}
+				found = false;
+			}*/
+			db.transaction(queryDB, errorCB);
+
 		} else {
 			console.log("Status: " + status);
 		}
 	});
-}
-function compareJSON(decodeTodoItems)
-{
-	var jsonItems = JSON.parse(decodeTodoItems);
-	console.log("Trying to parse JSON: " + decodeTodoItems + " with a length of " + jsonItems.length);
-	for(i = 0;i<jsonItems.length;i++)
-	{
-		id = jsonItems[i].id
-		hash = jsonItems[i].hash
-		timedate = jsonItems[i].timedate
-		owner = jsonItems[i].owner
-		completed = jsonItems[i].completed
-		description = jsonItems[i].description
-		console.log("Item: " + i + " - hash: " + hash + " - timedate: " + timedate + " - completed: " + completed + " - description: " + description);
-		/*
-		#######################################################################################################################
-		    Foreach row decoded from server, search the local database
-			- If not found, insert.
-			- If found, compare and update accordingly.
-		#######################################################################################################################
-		    Foreach row from local database, search the remote JSON values and add/remove/update remote values accordingly
-		#######################################################################################################################
-		*/
-	}
-
 
 }
