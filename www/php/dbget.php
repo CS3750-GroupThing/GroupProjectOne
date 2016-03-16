4997b01bee5e015cf28684be1516bd11c09f9d36
AaronLL <aaron@tadoo.net>
2016-03-16 08:38:48 -0600

    Added to test

---
+++ www/php/dbget.php
@@ -0,0 +1,117 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+	header('Access-Control-Max-Age: 1000');
+	header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+	/*
+		This form is looking for the $_POST['username'], $_POST['password'], $_POST['action'](GET,DELETE, ADD) and $_POST['data']
+		The actions below perform the following functions
+		- ADD - Inserts an entry in to todo_Items using the $_POST['data'] as the description
+		- DELETE - Deletes 1 entry from todo_Items where id=$_POST['data']
+		- GET - Returns all of the current entries belonging to $owner_ID in a JSON format
+	*/
+
+	/* For testing these values are in the Icarus database
+		Username: JoeBiden
+		Password: AnotherPassword encrypted into 8b5379d82d16e4c1fbe6aeb16b494da8bc11077571c994b47aafb8150abb4beeaa7ed43023edaebdfada54d003d402a1765a25e07f5b4009abbce83eb8acb19a
+
+		Username: TestOwner
+		Password: MyPassword encrypted into 7faa7303f769ce1d1f9c88bb669fbad62320c09eb131a47d78119842918853235f862caf9d9b2d6191cb03dad28609605c3f9298a8b81ab68674253e9e71d076
+	*/
+
+
+	/*
+		Examples of database inserts for MysQL
+		ID 1: INSERT INTO `W00353910`.`todo_Users`(`username`, `password`) VALUES ("TestOwner","7faa7303f769ce1d1f9c88bb669fbad62320c09eb131a47d78119842918853235f862caf9d9b2d6191cb03dad28609605c3f9298a8b81ab68674253e9e71d076")
+		ID 2: INSERT INTO `W00353910`.`todo_Users`(`username`, `password`) VALUES ("JoeBiden","8b5379d82d16e4c1fbe6aeb16b494da8bc11077571c994b47aafb8150abb4beeaa7ed43023edaebdfada54d003d402a1765a25e07f5b4009abbce83eb8acb19a")
+
+		INSERT INTO `todo_Items` (`owner`, `completed`, `description`) VALUES ('1', '0', 'Joe Biden Todo #1');
+		INSERT INTO `todo_Items` (`owner`, `completed`, `description`) VALUES ('2', '0', 'NewOwner Todo #1');
+	*/
+
+	/* VERIFY A USERNAME, PASSWORD AND ACTION WERE SUPPLIED VIA POST submission */
+	if (isset($_GET['username']) && isset($_GET['password']) && isset($_GET['action']))
+	{
+
+
+		/* INITIALIZE ICARUS DB LOGIN INFORMATION */
+		$servername = "localhost";
+		$username = "W00353910";
+		$password = "Aaroncs!";
+		$dbName = "W00353910";
+
+		$post_Username = $_GET['username'];
+		$post_Password = $_GET['password'];
+		$post_Action = $_GET['action'];
+		if ($post_Action == "ADD")
+		{
+			if ((isset($_GET['hash'])) && (isset($_GET['data'])) && (isset($_GET['completed'])))
+			{
+				$post_Hash = $_GET['hash'];
+				$post_Data = $_GET['data'];
+				$post_Completed = $_GET['completed'];
+			} else {
+				die("Invalid Add request.");
+			}
+
+		} else if ($post_Action == "DELETE")
+		{
+			if (isset($_GET['hash']))
+			{
+				$post_Hash = $_GET['hash'];			
+			} else {
+				die("Invalid delete request.");
+			}
+		}
+
+		// Create connection
+		$connection = mysqli_connect($servername, $username, $password, $dbName);
+
+		// Check connection
+		if (!$connection) {
+			die("Failure: Connection error");
+		}
+		
+		/*echo "Connected successfully";*/
+		$sql = "SELECT id, username, password FROM `todo_Users` WHERE username='$post_Username' LIMIT 1";
+		
+		$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+		//$result = mysqli_query($connection, $sql) or die(json_encode('{\"response\":\"failure",\"reason\":\"" . mysqli_error($connection) . "\"}'));
+
+		$row = mysqli_fetch_assoc($result);
+		$owner_ID = $row['id'];
+
+		/* VERIFY $_POST USERNAME AND PASSWORD AGAINST DATABASE USERNAME AND PASSWORD */
+		if (($row['username'] == $post_Username) && ($row['password'] == $post_Password))
+		{
+			if ($post_Action == "GET") 
+			{
+				// Fetch table rows from mysql db
+				$sql = "SELECT * FROM `todo_Items` WHERE owner=".$owner_ID;
+				$result = mysqli_query($connection, $sql) or die("Error: " . mysqli_error($connection));
+				$emparray = array();
+				while($row=mysqli_fetch_assoc($result))
+				{
+					$emparray[] = $row;
+				}
+				/* Return values in a JSON format */
+				die(json_encode($emparray));
+			} else if ($post_Action == "DELETE") {
+				$sql = "DELETE FROM `todo_Items` WHERE owner=".$owner_ID." AND hash='".$post_Hash."'";
+				echo $sql."<br /><br />";
+				$result = mysqli_query($connection, $sql) or die("Error: " . mysqli_error($connection));
+			} else if ($post_Action == "ADD") {
+				$sql = "INSERT INTO `todo_Items` (`owner`, `completed`, `hash`, `description`) VALUES ($owner_ID, $post_Completed, '$post_Hash','$post_Data');";
+				$result = mysqli_query($connection, $sql) or die("Error: " . mysqli_error($connection));
+			} else {
+				die("Failure: Invalid Action");
+			}
+
+		} else {
+			http_response_code(401);
+			die("Failure: Invalid username/password");
+		}
+	} else {
+		die(json_encode('{"response":"failure","reason":"invalid post data"}'));
+	}
+?>
