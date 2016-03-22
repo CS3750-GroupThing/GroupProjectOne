18941bb71bab4bee1988930f9e8fd92c76713539
scottschollmeyer <scottschollmeyer@mail.weber.edu>
2016-03-21 20:15:18 -0600

    new php file and reg

--- www/php/userData.php
+++ www/php/userData.php
@@ -3,25 +3,34 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-$db_server = mysqli_connect("localhost","scottschollmeyer","Ilovemy3pets!","CS3750");
+$servername = "localhost";
+$username = "W00353910";
+$password = "Aaroncs!";
+$dbName = "W00353910";
+$db_server = mysqli_connect($servername,$username,$password,$dbName);
 if(!$db_server) die("Unable to connect to MYSQL:" . mysql_error());
 
-var_dump(isset($_POST['action']));
 
 if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['action'])){
 	$userName = $_POST['username'];
 	$passWord = $_POST['password'];
 	if($_POST['action'] == "add")
 	{
-		$sql_query = "INSERT INTO users (username, password) VALUES('".$userName."','".$passWord."')";
+		$sql_query = "INSERT INTO todo_Users (username, password) VALUES('".$userName."','".$passWord."')";
 	    $sql = mysqli_query($db_server, $sql_query);
 	}elseif ($_POST['action'] == "get"){
-		$sql_query = "SELECT FROM users WHERE username = '".$userName."' AND password ='" .$passWord."' ";
+		$sql_query = "SELECT FROM todo_Users WHERE username = '".$userName."' AND password ='" .$passWord."' ";
 	    $sql = mysqli_query($db_server, $sql_query);
+		if($sql){
+			http_response_code(201);
+			die("Success");
+		}else{
+			http_response_code(401);
+			die("Failure");
+		}
 	}
         
-echo $_POST['username'];
+
 }
 
 
